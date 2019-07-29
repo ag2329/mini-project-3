3ca68d82aab8401887fa944151336500e0dc20ff
prajjavalgupta <prajjwal.gupta531@gmail.com>
2019-07-28 18:57:00 -0700

    initial commit for project

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class HomeController extends Controller
 {
     /**
@@ -21,8 +21,10 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
-        return view('home');
-    }
+     public function index()
+     {
+         $user = Auth::user();
+         $questions = $user->questions()->paginate(6);
+         return view('home')->with('questions', $questions);
+     }
 }
