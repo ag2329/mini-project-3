53ec36cd4eeb0818a3d43a2dfd42d789ab2eed56
prajjavalgupta <prajjwal.gupta531@gmail.com>
2019-08-07 18:49:21 -0700

    Added a fix in routes

--- routes/web.php
+++ routes/web.php
@@ -30,11 +30,3 @@ Route::delete('/questions/{question_id}/answer/{answer_id}', 'AnswerController@d
 Route::resources([
 'questions' => 'QuestionController',
 ]);
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
